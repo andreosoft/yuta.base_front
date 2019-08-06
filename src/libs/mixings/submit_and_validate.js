cc8793d48aebd9d1ad359fc6ab21a97c616eb828
Andrei <andrei@andrey-main>
2019-08-06 09:49:45 +0300

    Update 06.08

---
+++ src/libs/mixings/submit_and_validate.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import mixingValidator from "@/libs/validators";
+
+export default {
+    data: function() {
+        return {
+            api: null,
+            fields: null,
+            validators: null,
+            errors: null,
+            loading: false
+        };
+    },
+    methods: {
+        submitForm: function() {
+            if (this.validateAll(this.fields)) {
+                this.submit(this.fields, this.api);
+                return true;
+            }
+            return false;
+        },
+        submit: function(fields, api) {
+            this.loading = true;
+            axios
+                .post(api, fields)
+                .then(response => {
+                    this.loading = false;
+                    this.fields = response.data.data;
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        },
+        validateAll: function(fields) {
+            var noerror = true;
+            for (var index in fields) {
+                if (this.validate(index, fields[index])) {
+                    noerror = false;
+                }
+            }
+            return noerror;
+        },
+        validate: function(field, value) {
+            if (this.validators[field]) {
+                return (this.errors[field] = this.validator(
+                    this.validators[field],
+                    value
+                ));
+            }
+        }
+    }
+};
