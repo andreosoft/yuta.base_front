cc4cc9448cb32dc20a0c57c810601b7fee51dd5a
Denis Povazhnyy <d.povazhnyy@MacBook-Air-User-4.local>
2019-08-14 21:12:00 +0300

    Update view.vue

--- src/views/config/building/view.vue
+++ src/views/config/building/view.vue
@@ -14,9 +14,9 @@
 
     <div>
       <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-2">
           <div class="card">
-            <h1>Шахматка</h1>
+            <h1>Помещения</h1>
             <div class="card-body">
               <router-link
                 :to="{ name: 'config_sections_view', params: { id: fields.id }}"
