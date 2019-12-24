4b8b21f43f2f21f1aad5a062359c799305f12961
Denis Povazhnyy <d.povazhnyy@MacBook-Air-User-4.local>
2019-12-24 21:47:28 +0300

    Update index.vue

--- src/views/tasks/index.vue
+++ src/views/tasks/index.vue
@@ -2,13 +2,72 @@
  <div> <form1 :breadcrumbs="[{url: '#/', title: 'Домой'}, {url: '', title: title}]" :loading="loading">
     <template v-slot:title>{{title}}</template>
 </form1>
+<h4 class="pb-2 mb-0">Просроченные задачи</h4>
+<table class="table table-striped">
+  <thead>
+    <tr class="bg-danger">
+      <th scope="col">#</th>
+      <th scope="col">Имя</th>
+      <th scope="col">Фамилия</th>
+      <th scope="col">Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="table-danger">
+      <th scope="row"></th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr class="table-danger">
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr class="table-danger">
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+  </table>
+<h4 class="pb-2 mb-0">Сегодня</h4>
+<table class="table table-striped">
+  <thead>
+    <tr class="bg-success">
+      <th scope="col">#</th>
+      <th scope="col">Имя</th>
+      <th scope="col">Фамилия</th>
+      <th scope="col">Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="table-success">
+      <th scope="row"></th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr class="table-success">
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr class="table-success">
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+  </table>
 
-<div class="progress">
-  <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">Первичный контакт</div>
-  <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">Ждем ответ</div>
-  <div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">Заключение договора</div>
-</div>
-
+<h4 class="pb-2 mb-0">Завтра</h4>
+<h4 class="pb-2 mb-0">На этой недели</h4>
+<h4 class="pb-2 mb-0">На следующей недели</h4>
 
 
 
